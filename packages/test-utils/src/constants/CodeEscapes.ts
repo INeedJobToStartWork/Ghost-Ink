b260ac5ed510471132c85610bd49664ccfb69a36
ineedjobtostartwork <ineedjobtostartwork@gmail.com>
2025-10-11 19:43:10 +0200

    🎨 Style(test-utils): formatting

--- packages/test-utils/src/constants/CodeEscapes.ts
+++ packages/test-utils/src/constants/CodeEscapes.ts
@@ -3,29 +3,28 @@
 //----------------------
 
 /**
- * Code Escapes 
+ * Code Escapes
  * @internal @dontexport
  */
 export const CODE_ESCAPES = {
-  Backspace: "\b",
-  Tab: "\t",
-  Enter: "\n",
-  Return: "\r",
-  Escape: "\u001b",
-  Space: " ",
+	Backspace: "\b",
+	Tab: "\t",
+	Enter: "\n",
+	Return: "\r",
+	Escape: "\u001b",
+	Space: " ",
 
-  Delete: "\u007f",
-  Insert: "\u0016",
-  Home: "\u0001",
-  End: "\u0005",
-  PageUp: "\u0010",
-  PageDown: "\u0004",
+	Delete: "\u007f",
+	Insert: "\u0016",
+	Home: "\u0001",
+	End: "\u0005",
+	PageUp: "\u0010",
+	PageDown: "\u0004",
 
-  ArrowUp: "\u001b[A",
-  ArrowDown: "\u001b[B",
-  ArrowLeft: "\u001b[D",
-  ArrowRight: "\u001b[C"
+	ArrowUp: "\u001b[A",
+	ArrowDown: "\u001b[B",
+	ArrowLeft: "\u001b[D",
+	ArrowRight: "\u001b[C"
 } as const;
-
 
 export default CODE_ESCAPES;
